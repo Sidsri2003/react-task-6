aab39badf8e61c63bfd990be8134084c33530b17
Sidsri2003 <144169444+Sidsri2003@users.noreply.github.com>
2023-10-15 10:19:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Header from 'src/Components/Header.js';
+import Header from './src/Components/Header.js';
 import About from 'src/Components/About.js';
 import Projects from 'src/Components/Project.js';
 import Contact from 'src/Components/Contact.js';
