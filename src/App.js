37af87595fb36fa7727c1c4f8158b3452d888871
Sidsri2003 <144169444+Sidsri2003@users.noreply.github.com>
2023-10-15 10:17:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
-import Header from '/Users/arneshpal/Downloads/react-task-6/src/Components/Header.js';
+import Header from 'src/Components/Header.js';
 import About from 'src/Components/About.js';
-import Projects from '/Users/arneshpal/Downloads/react-task-6/src/Components/Project.js';
+import Projects from 'src/Components/Project.js';
 import Contact from 'src/Components/Contact.js';
 
 function App() {
