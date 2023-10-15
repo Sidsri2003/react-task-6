f3adf2e393b6f7f8101508294a91080d56103047
Sidsri2003 <144169444+Sidsri2003@users.noreply.github.com>
2023-10-15 10:14:52 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import Header from '/Users/arneshpal/Downloads/react-task-6/src/Components/Header.js';
+import About from '/Users/arneshpal/Downloads/react-task-6/src/Components/About.js';
+import Projects from '/Users/arneshpal/Downloads/react-task-6/src/Components/Project.js';
+import Contact from '/Users/arneshpal/Downloads/react-task-6/src/Components/Contact.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <About />
+      <Projects />
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
+
